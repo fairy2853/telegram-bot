0ba46f25ffb6c1f040d8fef48c7151b4dc84e2d8
fairy2853 <netakuca@gmail.com>
2025-05-26 16:13:25 +0300

    fixed user questions

--- bot/handlers/user_questions.py
+++ bot/handlers/user_questions.py
@@ -4,8 +4,9 @@ from functools import partial
 
 
 async def handle_user_question(
-    update: Update, context: ContextTypes.DEFAULT_TYPE, genai
+    update: Update, context: ContextTypes.DEFAULT_TYPE, genai, user_states
 ):
+    user_id = update.message.from_user.id
     user_message = update.message.text
     # Ігноруємо команди типу /start
     if user_message.startswith("/"):
@@ -21,8 +22,23 @@ async def handle_user_question(
         )
         response = model.generate_content(prompt)
         reply_text = response.text
+
     except Exception as e:
         reply_text = "Вибач, наразі я не можу відповісти на це питання"
         print(e)
 
     await update.message.reply_text(reply_text)
+
+    state = user_states.get(user_id)
+
+    if state == "waiting_passport":
+        await update.message.reply_text(
+            "📷 Якщо у вас більше немає питань, будь ласка, надішліть фото *паспорта*",
+            parse_mode="Markdown",
+        )
+
+    elif state == "waiting_car_doc":
+        await update.message.reply_text(
+            "📷 Якщо немає додаткових питань, надішліть фото *техпаспорта*",
+            parse_mode="Markdown",
+        )
